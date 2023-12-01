2398ebac47e600ed9308ec38c64a787e0e0be02d
SevenF7 <89695795+SevenF7@users.noreply.github.com>
2023-12-01 12:16:42 +0800

    21301175-郑美仑实验4

--- user/target/riscv64gc-unknown-none-elf/release/deps/03test-e86437b8bcb0de7b.d
+++
@@ -1,5 +0,0 @@
-/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03test-e86437b8bcb0de7b: src/bin/03test.rs
-
-/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/03test-e86437b8bcb0de7b.d: src/bin/03test.rs
-
-src/bin/03test.rs:
